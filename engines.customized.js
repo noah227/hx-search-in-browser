a253190ea3053bd2b4cc1662f1c08df05dec9900
noah227 <noah227@foxmail.com>
2024-07-15 11:29:51 +0800

    chore: 精简内置引擎

--- engines.customized.js
+++ engines.customized.js
@@ -1,13 +1,56 @@
 /**
- * 内置引擎ID列表：Baidu | Sogou | Bing | Google | 360 | cnblogs | csdn | github | stackoverflow
+ * 内置引擎ID列表：Baidu | Bing | Google
  * 引擎ID冲突时，自定义引擎优先级更高
- * @type {{template: string, templateMobile?: string, title: string, id: string}[]}
+ * @type {{template: string, templateMobile?: string, title: string, id: string, disabled?: boolean}[]}
  */
-module.exports = [
+module.exports = [ 
+	/** 已内置 */
 	// {
 	// 	template: "https://www.baidu.com/s?wd=%s",
 	// 	templateMobile: "https://m.baidu.com/s?wd=%s",
 	// 	title: "百度",
 	// 	id: "Baidu"
+	// }, 
+	// {
+	// 	template: "https://www.bing.com/search?q=%s", 
+	// 	title: "Bing",
+	// 	id: "Bing"
+	// },
+	// {
+	// 	template: "https://www.google.com/search?q=%s",
+	// 	title: "Google",
+	// 	id: "Google"
+	// },
+	/** 其他样例 */
+	// {
+	// 	template: "https://www.sogou.com/web?query=%s",
+	// 	templateMobile: "https://m.sogou.com/web/searchList.jsp?keyword=%s",
+	// 	title: "搜狗",
+	// 	id: "Sogou"
+	// },
+	// {
+	// 	template: "https://www.so.com/s?q=%s",
+	// 	title: "360",
+	// 	id: "360"
+	// },
+	// {
+	// 	template: "https://zzk.cnblogs.com/s?w=%s",
+	// 	title: "cnblogs",
+	// 	id: "cnblogs"
+	// },
+	// {
+	// 	template: "https://so.csdn.net/so/search?q=%s",
+	// 	title: "csdn",
+	// 	id: "csdn"
+	// },
+	// {
+	// 	template: "https://github.com/search?q=vuejs",
+	// 	title: "github",
+	// 	id: "github"
+	// },
+	// {
+	// 	template: "https://stackoverflow.com/search?q=%s",
+	// 	title: "stackoverflow",
+	// 	id: "stackoverflow"
 	// },
 ]
--- engines.js
+++ engines.js
@@ -7,13 +7,7 @@ module.exports = [
 		templateMobile: "https://m.baidu.com/s?wd=%s",
 		title: "百度",
 		id: "Baidu"
-	},
-	{
-		template: "https://www.sogou.com/web?query=%s",
-		templateMobile: "https://m.sogou.com/web/searchList.jsp?keyword=%s",
-		title: "搜狗",
-		id: "Sogou"
-	},
+	}, 
 	{
 		template: "https://www.bing.com/search?q=%s", 
 		title: "Bing",
@@ -23,30 +17,5 @@ module.exports = [
 		template: "https://www.google.com/search?q=%s",
 		title: "Google",
 		id: "Google"
-	},
-	{
-		template: "https://www.so.com/s?q=%s",
-		title: "360",
-		id: "360"
-	},
-	{
-		template: "https://zzk.cnblogs.com/s?w=%s",
-		title: "cnblogs",
-		id: "cnblogs"
-	},
-	{
-		template: "https://so.csdn.net/so/search?q=%s",
-		title: "csdn",
-		id: "csdn"
-	},
-	{
-		template: "https://github.com/search?q=vuejs",
-		title: "github",
-		id: "github"
-	},
-	{
-		template: "https://stackoverflow.com/search?q=%s",
-		title: "stackoverflow",
-		id: "stackoverflow"
-	},
+	}
 ]
